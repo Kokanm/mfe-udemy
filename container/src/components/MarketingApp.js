b30582d840f2d0f44eb74ca2659336dcc2e72287
Kokan Malenko <kokan_malenko@outlook.com>
2022-12-09 02:17:57 +0100

    Initial commit

---
+++ container/src/components/MarketingApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { mount } from "marketing/MarketingApp";
+
+export default function MarketingApp() {
+  const ref = React.useRef(null);
+
+  React.useEffect(() => {
+    mount(ref.current);
+  }, []);
+
+  return <div ref={ref} />;
+}
